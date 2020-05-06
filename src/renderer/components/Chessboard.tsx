2d84c27867abcf9bcd7b913d7250fbc4339252da
Kevin Ludwig <kevin.j.ludwig@disney.com>
2020-05-06 13:52:08 -0700

    clean up square styling

--- src/renderer/components/Chessboard.tsx
+++ src/renderer/components/Chessboard.tsx
@@ -22,6 +22,7 @@ interface Props {
     setMove: (any) => void;
     priorMove: any;
 }
+
 export default React.memo((props: Props) => {
     const {
         fen,
@@ -29,31 +30,7 @@ export default React.memo((props: Props) => {
         priorMove
     } = props;
     const [dropSquareStyle, setDropSquareStyle] = useState({});
-    const [squareStyles, setSquareStyles] = useState({});
     const [pieceSquare, setPieceSquare] = useState('');
-
-    const removeHighlightSquare = (square) => setSquareStyles(squareStyling({pieceSquare: square, priorMove}));
-
-    const highlightSquare = (sourceSquare, squaresToHighlight) => {
-        const highlightStyles = [sourceSquare, ...squaresToHighlight].reduce(
-            (a, c) => {
-                return {
-                    ...a,
-                    ...{
-                        [c]: {
-                            background: 'radial-gradient(circle, #fffc00 36%, transparent 40%)',
-                            borderRadius: '50%'
-                        }
-                    },
-                    ...squareStyling({
-                        priorMove,
-                        pieceSquare
-                    })
-                };
-            }, {});
-
-        setSquareStyles({...squareStyles, ...highlightStyles});
-    };
 
     const onDrop = ({ sourceSquare, targetSquare }) => {
         const move = chess.move({
@@ -64,28 +41,14 @@ export default React.memo((props: Props) => {
 
         if (move) {
             setMove(move);
-            setSquareStyles(squareStyling({pieceSquare, priorMove}));
         }
     };
 
-    const onMouseOverSquare = square => {
-        const moves = chess.moves({fen, square});
-        if (moves.length) {
-            highlightSquare(square, moves.map(m => m.to));
-        }
-    };
-
-    const onMouseOutSquare = square => removeHighlightSquare(square);
-
-    const onDragOverSquare = square => {
-        const isCentralSquare = ['e4', 'd4', 'e5', 'd5'].includes(square);
-        setDropSquareStyle(isCentralSquare ?
-            { backgroundColor: 'cornFlowerBlue' } :
-            { boxShadow: 'inset 0 0 1px 4px rgb(255, 255, 0)' });
+    const onDragOverSquare = () => {
+        setDropSquareStyle({ boxShadow: 'inset 0 0 1px 4px rgb(255, 255, 0)' });
     };
 
     const onSquareClick = square => {
-        setSquareStyles(squareStyling({ pieceSquare: square, priorMove }));
         setPieceSquare(square);
 
         const move = chess.move({
@@ -100,8 +63,6 @@ export default React.memo((props: Props) => {
         }
     };
 
-    const onSquareRightClick = square => setSquareStyles({ [square]: { backgroundColor: 'deepPink' }});
-
     const onCalcWidth = ({screenWidth, screenHeight}) => {
         const RESERVED = 100; /* roughly the size of the player button bar */
         return screenHeight >= (screenWidth + RESERVED) ?  screenWidth : screenHeight - RESERVED;
@@ -112,17 +73,14 @@ export default React.memo((props: Props) => {
             calcWidth={onCalcWidth}
             position={fen}
             onDrop={onDrop}
-            onMouseOverSquare={onMouseOverSquare}
-            onMouseOutSquare={onMouseOutSquare}
             boardStyle={{
               borderRadius: '5px',
               boxShadow: '0 5px 15px rgba(0, 0, 0, 0.5)'
             }}
-            squareStyles={squareStyles}
+            squareStyles={squareStyling({pieceSquare, priorMove})}
             dropSquareStyle={dropSquareStyle}
             onDragOverSquare={onDragOverSquare}
             onSquareClick={onSquareClick}
-            onSquareRightClick={onSquareRightClick}
             transitionDuration={100}
             undo
         />
